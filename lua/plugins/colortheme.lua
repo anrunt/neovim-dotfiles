c8512910fcd96c976ca57e11b2b0bc18bbf5cad6
anrunt <grunt.janek@gmail.com>
2024-07-16 23:07:01 +0200

    New colorscheme and cleared unecessary plugins

--- lua/plugins/colortheme.lua
+++ lua/plugins/colortheme.lua
@@ -1,19 +1,9 @@
 return {
-  "folke/tokyonight.nvim",
+  "vague2k/vague.nvim",
   lazy = false,
   priority = 1000,
   opts = {},
   config = function()
-    require("tokyonight").setup({
-      styles = {
-        comments = { italic = false },
-        keywords = { italic = false },
-        functions = { italic = false },
-        variables = { italic = false },
-        sidebars = "dark",
-        floats = "dark",
-      },
-    })
-    vim.cmd.colorscheme("tokyonight-night")
+    vim.cmd.colorscheme("vague")
   end,
 }
