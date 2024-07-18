9436ab0ed1ad6da7e667c9fb51d1013474c54ee4
anrunt <grunt.janek@gmail.com>
2024-07-18 22:49:27 +0200

    Small changes

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,66 +1,90 @@
 return {
-	{
-		"williamboman/mason.nvim",
-		lazy = false,
-		config = function()
-			require("mason").setup()
-		end,
-	},
-	{
-		"williamboman/mason-lspconfig.nvim",
-		lazy = false,
-		config = function()
-			require("mason-lspconfig").setup({
-				ensure_installed = { "lua_ls", "svelte", "tailwindcss", "html", "cssls" },
-			})
-		end,
-	},
-	{
-		"neovim/nvim-lspconfig",
-		lazy = false,
-		config = function()
-			local capabilities = require("cmp_nvim_lsp").default_capabilities()
-			local lspconfig = require("lspconfig")
+  {
+    "williamboman/mason.nvim",
+    lazy = false,
+    config = function()
+      require("mason").setup()
+    end,
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    lazy = false,
+    config = function()
+      require("mason-lspconfig").setup({
+        ensure_installed = { "lua_ls", "svelte", "tailwindcss", "html", "cssls" },
+      })
+    end,
+  },
+  {
+    "neovim/nvim-lspconfig",
+    lazy = false,
+    config = function()
+      local capabilities = require("cmp_nvim_lsp").default_capabilities()
+      local lspconfig = require("lspconfig")
 
-			lspconfig.lua_ls.setup({
-				capabilities = capabilities,
-			})
+      lspconfig.lua_ls.setup({
+        capabilities = capabilities,
+      })
 
-			--			lspconfig.tsserver.setup({
-			--				capabilities = capabilities,
-			--				root_dir = require("lspconfig.util").root_pattern(".git"),
-			--			})
+      --			lspconfig.tsserver.setup({
+      --				capabilities = capabilities,
+      --				root_dir = require("lspconfig.util").root_pattern(".git"),
+      --			})
 
-			lspconfig.svelte.setup({
-				capabilities = capabilities,
-				root_dir = require("lspconfig.util").root_pattern(".git"),
-			})
+      lspconfig.svelte.setup({
+        capabilities = capabilities,
+        root_dir = require("lspconfig.util").root_pattern(".git"),
+      })
 
-			lspconfig.tailwindcss.setup({
-				capabilities = capabilities,
-				root_dir = require("lspconfig.util").root_pattern(".git"),
-			})
+--      lspconfig.tailwindcss.setup({
+--        capabilities = capabilities,
+--        root_dir = require("lspconfig.util").root_pattern(".git"),
+--      })
 
-			lspconfig.html.setup({
-				capabilities = capabilities,
-				root_dir = require("lspconfig.util").root_pattern(".git"),
-			})
+      lspconfig.tailwindcss.setup({
+        capabilities = capabilities,
+        root_dir = lspconfig.util.root_pattern(".git", "tailwind.config.js", "tailwind.config.ts"),
+        settings = {
+          tailwindCSS = {
+            experimental = {
+              classRegex = {
+                { "cva\\(([^)]*)\\)", "[\"'`]([^\"'`]*).*?[\"'`]" },
+                { "cx\\(([^)]*)\\)",  "(?:'|\"|`)([^']*)(?:'|\"|`)" },
+              },
+            },
+            validate = true,
+          },
+        },
+        filetypes = {
+          "html",
+          "javascriptreact",
+          "typescriptreact",
+          "javascript",
+          "typescript",
+          "svelte",
+        },
+      })
 
-			lspconfig.cssls.setup({
-				capabilities = capabilities,
-				root_dir = require("lspconfig.util").root_pattern(".git"),
-			})
+      lspconfig.html.setup({
+        capabilities = capabilities,
+        root_dir = require("lspconfig.util").root_pattern(".git"),
+      })
 
-			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
-			vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
-			vim.keymap.set("n", "<leader>e", "<cmd>lua vim.diagnostic.open_float()<CR>", {})
-		end,
-	},
-	{
-		"pmizio/typescript-tools.nvim",
-		lazy = false,
-		dependencies = { "nvim-lua/plenary.nvim", "neovim/nvim-lspconfig" },
-		opts = {},
-	},
+      lspconfig.cssls.setup({
+        capabilities = capabilities,
+        root_dir = require("lspconfig.util").root_pattern(".git"),
+      })
+
+      vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+      vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+      vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
+      vim.keymap.set("n", "<leader>e", "<cmd>lua vim.diagnostic.open_float()<CR>", {})
+    end,
+  },
+  {
+    "pmizio/typescript-tools.nvim",
+    lazy = false,
+    dependencies = { "nvim-lua/plenary.nvim", "neovim/nvim-lspconfig" },
+    opts = {},
+  },
 }
--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -3,7 +3,7 @@ vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
 -- Sets cursor to be block always
---vim.cmd("set guicursor=n-v-c-i:block")
+vim.cmd("set guicursor=n-v-c-i:block")
 vim.opt.wrap = false
 vim.g.mapleader = " "
 vim.opt.number = true
@@ -12,3 +12,5 @@ vim.wo.relativenumber = true
 vim.opt.scrolloff = 8
 vim.opt.colorcolumn = "80"
 vim.opt.termguicolors = true
+-- Lower updatetime
+vim.o.updatetime = 100
