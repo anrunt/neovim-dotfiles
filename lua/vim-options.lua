f9d5afbf3ac3c648b7481ecb7a8dc5a42138e7cc
anrunt <grunt.janek@gmail.com>
2024-08-20 23:53:44 +0200

    Updated plugins

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -3,7 +3,7 @@ vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
 -- Sets cursor to be block always
-vim.cmd("set guicursor=n-v-c-i:block")
+--vim.cmd("set guicursor=n-v-c-i:block")
 vim.opt.wrap = false
 vim.g.mapleader = " "
 vim.opt.number = true
