b1f45b2984b6d069cb5854e3f2af91bf96f2e89f
anrunt <grunt.janek@gmail.com>
2024-09-05 22:32:27 +0200

    Some changes

--- lua/plugins/colortheme.lua
+++ lua/plugins/colortheme.lua
@@ -5,27 +5,27 @@
 --  opts = {},
 --  config = function()
 --    require('solarized-osaka').setup({
---      transparent = true
+--      transparent = false
 --    })
 --    vim.cmd.colorscheme("solarized-osaka")
 --  end
 --}
 return {
-  "folke/tokyonight.nvim",
-  lazy = false,
-  priority = 1000,
-  opts = {},
-  config = function()
-    require("tokyonight").setup({
-      styles = {
-        comments = { italic = false },
-        keywords = { italic = false },
-        functions = { italic = false },
-        variables = { italic = false },
-        sidebars = "dark",
-        floats = "dark",
-      },
-    })
-    vim.cmd.colorscheme("tokyonight-night")
-  end,
+	"folke/tokyonight.nvim",
+	lazy = false,
+	priority = 1000,
+	opts = {},
+	config = function()
+		require("tokyonight").setup({
+			styles = {
+				comments = { italic = false },
+				keywords = { italic = false },
+				functions = { italic = false },
+				variables = { italic = false },
+				sidebars = "dark",
+				floats = "dark",
+      }
+		})
+		vim.cmd.colorscheme("tokyonight-night")
+	end,
 }
