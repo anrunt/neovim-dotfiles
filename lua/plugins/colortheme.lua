5414f165cb3729aa86abf5c91565d54ccfd044d8
anrunt <grunt.janek@gmail.com>
2024-09-29 22:38:40 +0200

    Changed cursor, deleted unused colorscheme

--- lua/plugins/colortheme.lua
+++ lua/plugins/colortheme.lua
@@ -1,15 +1,3 @@
---return {
---  "craftzdog/solarized-osaka.nvim",
---  lazy = false,
---  priority = 1000,
---  opts = {},
---  config = function()
---    require('solarized-osaka').setup({
---      transparent = false
---    })
---    vim.cmd.colorscheme("solarized-osaka")
---  end
---}
 return {
 	"folke/tokyonight.nvim",
 	lazy = false,
