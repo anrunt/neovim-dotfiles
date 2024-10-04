51f75e9e23f1d833f833aee2aacbdd41352e9999
anrunt <75736769+anrunt@users.noreply.github.com>
2024-10-04 23:33:42 +0200

    Deleted space

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -13,6 +13,7 @@ return {
         -- Python
         null_ls.builtins.formatting.black,
         null_ls.builtins.formatting.isort,
+
         -- TS etc.
         null_ls.builtins.formatting.prettier,
         require("none-ls.code_actions.eslint_d"),
