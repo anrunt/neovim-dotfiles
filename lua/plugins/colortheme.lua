c6cd4487377a72ccfc017f42fa15f13229ad3cd0
anrunt <grunt.janek@gmail.com>
2024-12-26 18:39:29 +0100

    Changes

--- lua/plugins/colortheme.lua
+++ lua/plugins/colortheme.lua
@@ -17,3 +17,13 @@ return {
 		vim.cmd.colorscheme("tokyonight-night")
 	end,
 }
+--return {
+--  "oxfist/night-owl.nvim",
+--  lazy = false, -- make sure we load this during startup if it is your main colorscheme
+--  priority = 1000, -- make sure to load this before all the other start plugins
+--  config = function()
+--    -- load the colorscheme here
+--    require("night-owl").setup()
+--    vim.cmd.colorscheme("night-owl")
+--  end
+--}
--- lua/plugins/completions.lua
+++ lua/plugins/completions.lua
@@ -1,45 +1,55 @@
 return {
-  {
-    "hrsh7th/cmp-nvim-lsp",
-  },
-  {
-    "github/copilot.vim",
-    config = function()
-      vim.g.copilot_clear_on_normal_mode = true -- Clear suggestions in normal mode 
-    end
-  },
-  {
-    "L3MON4D3/LuaSnip",
-    dependencies = {
-      "saadparwaiz1/cmp_luasnip",
-      "rafamadriz/friendly-snippets",
-    },
-  },
-  {
-    "hrsh7th/nvim-cmp",
-    config = function()
-      local cmp = require("cmp")
-      require("luasnip.loaders.from_vscode").lazy_load()
+	{
+		"hrsh7th/cmp-nvim-lsp",
+	},
+	{
+		"github/copilot.vim",
+		config = function()
+			vim.keymap.set("n", "<leader>ct", function()
+				if vim.g.copilot_enabled then
+					vim.g.copilot_enabled = false
+					print("Copilot suggestions disabled")
+				else
+					vim.g.copilot_enabled = true
+					print("Copilot suggestions enabled")
+				end
+			end, { desc = "Toggle Copilot suggestions" })
 
-      cmp.setup({
-        snippet = {
-          expand = function(args)
-            require("luasnip").lsp_expand(args.body)
-          end,
-        },
-        mapping = cmp.mapping.preset.insert({
-          ["<C-b>"] = cmp.mapping.scroll_docs(-4),
-          ["<C-f>"] = cmp.mapping.scroll_docs(4),
-          ["<C-Space>"] = cmp.mapping.complete(),
-          ["<C-e>"] = cmp.mapping.abort(),
-          ["<CR>"] = cmp.mapping.confirm({ select = true }),
-        }),
-        sources = cmp.config.sources({
-          { name = "nvim_lsp" },
-          { name = "luasnip" },
-          { name = "buffer" },
-        }),
-      })
-    end,
-  },
+			vim.g.copilot_clear_on_normal_mode = true -- Clear suggestions in normal mode
+		end,
+	},
+	{
+		"L3MON4D3/LuaSnip",
+		dependencies = {
+			"saadparwaiz1/cmp_luasnip",
+			"rafamadriz/friendly-snippets",
+		},
+	},
+	{
+		"hrsh7th/nvim-cmp",
+		config = function()
+			local cmp = require("cmp")
+			require("luasnip.loaders.from_vscode").lazy_load()
+
+			cmp.setup({
+				snippet = {
+					expand = function(args)
+						require("luasnip").lsp_expand(args.body)
+					end,
+				},
+				mapping = cmp.mapping.preset.insert({
+					["<C-b>"] = cmp.mapping.scroll_docs(-4),
+					["<C-f>"] = cmp.mapping.scroll_docs(4),
+					["<C-Space>"] = cmp.mapping.complete(),
+					["<C-e>"] = cmp.mapping.abort(),
+					["<CR>"] = cmp.mapping.confirm({ select = true }),
+				}),
+				sources = cmp.config.sources({
+					{ name = "nvim_lsp" },
+					{ name = "luasnip" },
+					{ name = "buffer" },
+				}),
+			})
+		end,
+	},
 }
